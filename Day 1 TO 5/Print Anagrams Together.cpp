18743b5c581adfd6ae01b04b0732fbf7929b2f83
Debojyoti1915001 <debojyoti76@gmail.com>
2022-01-02 13:13:27 +0530

    Task 1

---
+++ Day 1 TO 5/Print Anagrams Together.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& s) {
+        //code here
+        vector<vector<string> >ans;
+        unordered_map<string,vector<string>>um;
+        for(auto t:s){
+            string temp=t;
+            sort(temp.begin(),temp.end());
+            um[temp].push_back(t);
+        }
+        for(auto itr:um){
+            ans.push_back(itr.second);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i]; 
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
