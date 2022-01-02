e2b6ce2ea472f50b3e0ca6148df0edbd86d17f3f
Debojyoti1915001 <debojyoti76@gmail.com>
2022-01-02 13:56:27 +0530

    Task 1

--- Day 1 TO 5/Overlapping rectangles.cpp
+++ Day 1 TO 5/Overlapping rectangles.cpp
@@ -11,6 +11,7 @@ class Solution {
   public:
     int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
         // code here
+        //find the four conditions where overlaping is impossible 
         if(L1[0]>R2[0]||L2[0]>R1[0])return 0;
         if(L2[1]<R1[1]||L1[1]<R2[1])return 0;
         return 1;
