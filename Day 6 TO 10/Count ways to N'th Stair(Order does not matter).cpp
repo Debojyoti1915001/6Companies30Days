66f6e8dbe05cfff108978b0e044dcac860ae7a2d
Debojyoti1915001 <debojyoti76@gmail.com>
2022-01-11 02:01:28 +0530

    Task2

---
+++ Day 6 TO 10/Count ways to N'th Stair(Order does not matter).cpp
@@ -0,0 +1,42 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to count number of ways to reach the nth stair 
+    //when order does not matter.
+    long long countWays(int n)
+    {
+        // your code here
+        vector<int>dp(n+1);
+        dp[1]=1;
+        dp[0]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-2]+1;
+        }
+        return dp[n];
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking count of testcases
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        //taking stair count
+        int m;
+        cin>>m;
+        Solution ob;
+        cout<<ob.countWays(m)<<endl; // Print the output from our pre computed array
+    }
+    return 0;
+}  // } Driver Code Ends
