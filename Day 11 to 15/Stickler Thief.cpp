de072b52c5d997cd8472b9857e2a7c73d400bb50
Debojyoti1915001 <debojyoti76@gmail.com>
2022-01-18 21:07:46 +0530

    task 3

---
+++ Day 11 to 15/Stickler Thief.cpp
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        vector<int>dp(n+1,0);
+        dp[1]=arr[0];
+        for(int i=2;i<=n;i++){
+            dp[i]=max(dp[i-1],dp[i-2]+arr[i-1]);
+        }
+        return dp[n];
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking total testcases
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    //taking number of houses
+		int n;
+		cin>>n;
+		int a[n];
+		
+		//inserting money of each house in the array
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;
+		//calling function FindMaxSum()
+		cout<<ob.FindMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
